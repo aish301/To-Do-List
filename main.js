a6841603036b866484177e589e9fdbb7ecf9d142
Aishwarya <aishwarya.nr3@gmail.com>
2026-02-04 16:17:25 +0530

    Design

---
+++ main.js
@@ -0,0 +1,4 @@
+const inputBox = document.getElementById("taskId");
+const listContainer = document.getElementById("listId");
+const addTaskbtn = document.getElementById("addButton");
+const errorShow = document.getElementById("error");
